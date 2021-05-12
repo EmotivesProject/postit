ef2dd85c89b2cd810c2d0b45b27c20936ca87e56
Tom Bowyer <tom4310@gmail.com>
2021-05-12 19:18:47 +1000

    Add tests for creating comment and likes

--- test/internal/router_integration_test.go
+++ test/internal/router_integration_test.go
@@ -3,16 +3,21 @@
 package api_test
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
 	"postit/test"
 	"strings"
 	"testing"
+	"time"
 
 	commonTest "github.com/TomBowyerResearchProject/common/test"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRouterUserHandling(t *testing.T) {
+func TestRouterCreatePost(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	fmt.Printf("%d", time.Now().Unix())
 	test.SetUpIntegrationTest()
 
 	token := commonTest.CreateNewUser(t, "http://0.0.0.0:8082/user")
@@ -29,4 +34,64 @@ func TestRouterUserHandling(t *testing.T) {
 	assert.EqualValues(t, r.StatusCode, http.StatusCreated)
 
 	test.TearDownIntegrationTest()
+}
+
+func TestRouterCreateLike(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	fmt.Printf("%d", time.Now().Unix())
+	test.SetUpIntegrationTest()
+
+	token := commonTest.CreateNewUser(t, "http://0.0.0.0:8082/user")
+
+	id := createPost(t, token)
+
+	url := fmt.Sprintf("%s/post/%s/like", test.TS.URL, id)
+
+	req, _ := http.NewRequest("POST", url, nil)
+	req.Header.Add("Authorization", token)
+
+	r, _, _ := commonTest.CompleteTestRequest(t, req)
+
+	assert.EqualValues(t, r.StatusCode, http.StatusCreated)
+
+	test.TearDownIntegrationTest()
+}
+
+func TestRouterCreateComment(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+	fmt.Printf("%d", time.Now().Unix())
+	test.SetUpIntegrationTest()
+
+	token := commonTest.CreateNewUser(t, "http://0.0.0.0:8082/user")
+
+	id := createPost(t, token)
+
+	url := fmt.Sprintf("%s/post/%s/comment", test.TS.URL, id)
+
+	requestBody := strings.NewReader(
+		"{\"content\": {\"message\": \"HELLO\"} }",
+	)
+
+	req, _ := http.NewRequest("POST", url, requestBody)
+	req.Header.Add("Authorization", token)
+
+	r, _, _ := commonTest.CompleteTestRequest(t, req)
+
+	assert.EqualValues(t, r.StatusCode, http.StatusCreated)
+
+	test.TearDownIntegrationTest()
+}
+
+func createPost(t *testing.T, token string) string {
+	requestBody := strings.NewReader(
+		"{\"content\": {\"message\": \"HELLO\"} }",
+	)
+
+	req, _ := http.NewRequest("POST", test.TS.URL+"/post", requestBody)
+	req.Header.Add("Authorization", token)
+
+	_, resultMap, _ := commonTest.CompleteTestRequest(t, req)
+
+	intID := int64(resultMap["id"].(float64))
+	return fmt.Sprintf("%d", intID)
 }
