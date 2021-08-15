f278f8f0912c7c3773b44f5618c8eaa0e62ef351
Tom Bowyer <tom4310@gmail.com>
2021-08-15 16:18:19 +1000

    do some server side testing on comments and posts

--- model/PostInformation.go
+++ model/PostInformation.go
@@ -1,8 +1,9 @@
 package model
 
 type PostInformation struct {
-	Post       Post         `json:"post"`
-	Comments   []Comment    `json:"comments"`
-	EmojiCount []EmojiCount `json:"emoji_count"`
-	Likes      []Like       `json:"likes"`
+	Post           Post         `json:"post"`
+	Comments       []Comment    `json:"comments"`
+	EmojiCount     []EmojiCount `json:"emoji_count"`
+	SelfEmojiCount []EmojiCount `json:"self_emoji_count"`
+	Likes          []Like       `json:"likes"`
 }
